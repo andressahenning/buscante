53cae81a8d470fa72ae2701a36b689ed2a68e0d1
Andressa <henning.andressa1@gmail.com>
2025-10-01 20:44:47 -0300

    feat: adicionando tratamento de erros

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -13,13 +13,13 @@ export class LivroService {
     private http: HttpClient
   ) { }
 
-  buscar(valorDigitado: string): Observable<Item[]>{
+  buscar(valorDigitado: string): Observable<LivrosResultado>{
     const params = new HttpParams().append('q', valorDigitado)
-    return this.http.get<LivrosResultado>(this.API, { 
-      params }).pipe(
+    return this.http.get<LivrosResultado>(this.API, { params });
+      /*.pipe(
         // tap(retornoAPI => console.log('Fluxo do tap', retornoAPI)),
-        map(resultado => resultado.items),
+        map(resultado => resultado.items ?? []),
         // tap(resultado => console.log('Fluxo após o map', resultado))
-      )
+      )*/
   }
 }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,6 +1,6 @@
-import { Livro, Item } from './../../models/interfaces';
+import { Item, LivrosResultado } from './../../models/interfaces';
 import { Component } from '@angular/core';
-import { debounceTime, filter, map, switchMap, tap } from 'rxjs';
+import { catchError, debounceTime, EMPTY, filter, map, switchMap, distinctUntilChanged, tap, of } from 'rxjs';
 import { LivroService } from 'src/app/service/livro.service';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { FormControl } from '@angular/forms';
@@ -14,6 +14,8 @@ const PAUSE = 300;
 })
 export class ListaLivrosComponent {
   campoBusca = new FormControl();
+  mensagemErro = '';
+  totalDeLivros = 0;
 
   constructor(
     private service: LivroService
@@ -21,11 +23,22 @@ export class ListaLivrosComponent {
 
   // $ -> convensão para representar um observable
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
-    filter((valorDigitado) => valorDigitado.length >= 3),
     debounceTime(PAUSE),
-    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
-    map(items => this.livrosResultadoParaLivros(items))
-  )
+    filter(valorDigitado => valorDigitado.length >= 3),
+    distinctUntilChanged(),
+    switchMap(valorDigitado => this.service.buscar(valorDigitado)),
+    tap(resultado => {
+      this.totalDeLivros = resultado.totalItems ?? 0;
+      this.mensagemErro = this.totalDeLivros === 0 ? 'Nenhum resultado encontrado' : '';
+    }),
+    map(resultado => resultado.items ?? []),
+    map(items => this.livrosResultadoParaLivros(items)),
+    catchError(() => {
+      this.mensagemErro = 'Ops, ocorreu um erro. Recarregue a página';
+      // Retorna um array vazio para limpar a tela e manter o observable ativo
+      return of([]);
+    })
+  );
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
     return items.map(item => {
